69a403fbe9e5508436a705e0a087085d933c0799
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-16 18:41:19 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React, { useCallback } from 'react'
 import debug from 'debug'
+import { useKey, useGetSet } from 'react-use'
+import cx from 'clsx'
+import { taggedSum } from 'daggy'
+import over from 'ramda/es/over'
+import lensProp from 'ramda/es/lensProp'
+import pipe from 'ramda/es/pipe'
+import add from 'ramda/es/add'
+import mathMod from 'ramda/es/mathMod'
+import identity from 'ramda/es/identity'
+
+const overProp = propName => over(lensProp(propName))
+const nop = () => {}
 
 const log = debug('app:App.jsx:')
 
-function getMainItems() {
-  return [
+const initialState = {
+  hlIdx: 0,
+  items: [
     //
     { title: 'Show Date' },
     { title: 'Show Time' },
@@ -15,19 +27,79 @@ function getMainItems() {
         log('Show bookmarks list')
       },
     },
-  ]
+  ],
 }
+
+const Msg = taggedSum('Msg', {
+  INC: [],
+  DEC: [],
+})
+
+const update = msg => state => {
+  const rollHlIdxBy = offset => {
+    const totalItems = state.items.length
+    if (totalItems === 0) {
+      return identity
+    }
+    return overProp('hlIdx')(
+      pipe(
+        //
+        add(offset),
+        idx => mathMod(idx, totalItems),
+      ),
+    )
+  }
+  return msg.cata({
+    INC: () => [rollHlIdxBy(1)(state), Cmd.none],
+    DEC: () => [rollHlIdxBy(-1)(state), Cmd.none],
+  })
+}
+
+const Cmd = {
+  run: () => cmd => log(cmd),
+  none: nop,
+}
+
+const useStateEffect = initialState => update => {
+  const [get, set] = useGetSet(initialState)
+  const send = useCallback(
+    msg => {
+      const [nextState, cmd] = update(msg)(get())
+      set(nextState)
+      Cmd.run(send)(cmd)
+    },
+    [get, set, update],
+  )
+  return [get, send]
+}
+
 export function App() {
-  const mainItems = getMainItems()
+  const [get, send] = useStateEffect(initialState)(update)
+  const hlIdx = get().hlIdx
+  const cmdList = get().items
+
+  useKey('ArrowDown', () => {
+    send(Msg.INC)
+  })
+
+  useKey('ArrowUp', () => {
+    send(Msg.DEC)
+  })
+
   return (
     <div className="lh-copy measure-wide center">
       <div className="pv3 b ttu">Launcher</div>
+      <div className="pv2">Input: {`<type to filter>`}</div>
       <div>
-        {mainItems.map((cmd, idx) => {
+        {cmdList.map((cmd, idx) => {
+          const isHighlighted = idx === hlIdx
           return (
             <div
               key={idx}
-              className="pointer pa1 ba mv1"
+              className={cx(
+                'pointer pa1 ba mv1',
+                isHighlighted ? 'bg-light-gray' : '',
+              )}
               onClick={() => cmd.onSelect && cmd.onSelect()}
             >
               {cmd.title}
