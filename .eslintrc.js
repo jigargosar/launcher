ace3f8369a473c0d71890566904d530a56dd9089
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-16 16:27:04 +0530

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   plugins: [],
   rules: {
-    "react/prop-types": 0,
-    "react/display-name": 0
-  }
-};
+    'react/prop-types': 0,
+    'react/display-name': 0,
+  },
+}
