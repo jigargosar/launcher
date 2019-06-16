f5ddbc7bf1690a72ec9b5f9689f98ab64c1d2508
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-16 16:50:29 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
 import React from 'react'
 
+import debug from 'debug'
+
+const log = debug('app:App.jsx:')
+
+function getCmdList() {
+  return [
+    //
+    { title: 'Show Date' },
+    { title: 'Show Time' },
+    {
+      title: 'Web Bookmarks',
+      onSelect: () => {
+        log('Show bookmarks list')
+      },
+    },
+  ]
+}
 export function App() {
-  return <div>HW</div>
+  const cmdList = getCmdList()
+  return (
+    <div className="lh-copy measure-wide center">
+      <div className="pv3 b ttu">Launcher</div>
+      <div>
+        {cmdList.map((cmd, idx) => {
+          return (
+            <div
+              key={idx}
+              className="pointer pa1 ba mv1"
+              onClick={() => cmd.onSelect && cmd.onSelect()}
+            >
+              {cmd.title}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
