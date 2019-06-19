8c55c2ba2676baffea5ebaabc136ea5efa36f62d
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-19 16:03:43 +0530

    server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+/* eslint-env node */
+
+const express = require('express')
+const app = express()
+const port = 8081
+
+app.get('/', (req, res) => {
+  // res.json({ msg2: 'Hello World!' })
+  res.send({ msg: 'Hello World!' })
+})
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
+)
