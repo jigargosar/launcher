57e681e8801166fafed25775ef83ef3b47fbc931
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-24 16:12:16 +0530

    electron

--- src/electron/index.js
+++ src/electron/index.js
@@ -20,7 +20,7 @@ if (!gotTheLock) {
 
   app.quit()
 } else {
-  debug()
+  // debug()
   // Keep a global reference of the window object, if you don't, the window will
   // be closed automatically when the JavaScript object is garbage collected.
   let mainWindow
